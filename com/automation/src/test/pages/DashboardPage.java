61f5459f7766a43cc548445f49175b74806be971
tomashgombosh <blackjneco@gmail.com>
2019-12-04 11:56:54 +0200

    Change test loginc

--- com/automation/src/test/pages/DashboardPage.java
+++ com/automation/src/test/pages/DashboardPage.java
@@ -1,14 +1,10 @@
 package pages;
 
-import com.google.gson.JsonObject;
 import common.PageElement;
 import common.Utils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.HashMap;
 
 public class DashboardPage extends BasePage {
     private static final PageElement searchFld = new PageElement(
@@ -50,19 +46,16 @@ public class DashboardPage extends BasePage {
 
     public void collectData() {
         Utils.sleep(4000);
-        for (int next = 1; next <= 10; next++) {
-            for (int counter = 1; counter <= 10; counter++)
-                collect(counter);
+            collect();
             scrollToView(nextButton);
             click(nextButton);
-        }
     }
 
-    private void collect(int counter) {
-        PageElement name = new PageElement("Name of employee", By.cssSelector("ul.search-results__list li:nth-child(" + counter + ") div:nth-child(2) [data-control-name='search_srp_result']"));
-        PageElement position = new PageElement("Position of employee", By.cssSelector("ul.search-results__list li:nth-child(" + counter + ") div:nth-child(2)  p:nth-child(2)"));
-        PageElement country = new PageElement("Country of employee", By.cssSelector("ul.search-results__list li:nth-child(" + counter + ") div:nth-child(2)  p:nth-child(3)"));
-        PageElement currentWorking = new PageElement("Country of employee", By.cssSelector("ul.search-results__list li:nth-child(" + counter + ") div:nth-child(2)  p:nth-child(4)"));
+    private void collect() {
+        PageElement name = new PageElement("Name of employee", By.cssSelector("ul.search-results__list li:nth-child(1) div:nth-child(2) [data-control-name='search_srp_result']"));
+        PageElement position = new PageElement("Position of employee", By.cssSelector("ul.search-results__list li:nth-child(1) div:nth-child(2)  p:nth-child(2)"));
+        PageElement country = new PageElement("Country of employee", By.cssSelector("ul.search-results__list li:nth-child(1) div:nth-child(2)  p:nth-child(3)"));
+        PageElement currentWorking = new PageElement("Country of employee", By.cssSelector("ul.search-results__list li:nth-child(1) div:nth-child(2)  p:nth-child(4)"));
         scrollToView(name);
         log.info(getText(name));
         log.info(getText(position));
